e7c4fd447236dabe7b36a91709dd4093bb73388d
Steffi Eaint <t3moea00@students.oamk.fi>
2024-04-07 22:10:16 +0300

    added password hashing

--- server/login.sql
+++ server/login.sql
@@ -1,8 +1,9 @@
 
-create table account (
+create table users (
   id serial primary key,
+  username varchar(100) unique not null,
   email varchar(100) unique not null,
   password varchar(255) not null
 );
 
-insert into account (email,password) values ('admin@foo.com','admin123');
+insert into users (username,email,password) values ('admin','admin@123','admin123');
